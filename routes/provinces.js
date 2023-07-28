b25e31e4d27f31cbd92ab67d7300a78a911b9111
yuki-UwU <raoseta@gmail.com>
2023-07-28 21:27:22 +0300

    nothing

--- routes/provinces.js
+++ routes/provinces.js
@@ -10,7 +10,7 @@ const router = express.Router();
   return a list of province
 */
 router.get("/", auth, async (req, res) => {
-  const provinces = await Province.get();
+  const provinces = await Province.find();
 
   const customResponse = new CustomResponse(200, '', provinces);
   res.send(customResponse);
