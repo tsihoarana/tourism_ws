dc7f341d444a592c68b28b8d855ebe635b67d258
yuki-UwU <raoseta@gmail.com>
2023-07-28 19:34:19 +0300

    [EDIT] lieu

--- models/lieu.js
+++ models/lieu.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const config = require("config");
 
 const lieuSchema = new mongoose.Schema({
   idprovince: {
@@ -16,8 +17,19 @@ const lieuSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 512
   },
+  pdp: { type: String },
+  pdc: { type: String },
   images: [String]
 });
+
+lieuSchema.statics.get = async function (query) {
+  let lieus = await this.find(query).lean().exec();
+  
+  return lieus.map(obj => ({ ...obj, 
+                    pdp: config.get("media_url") + obj.pdp,
+                    pdc: config.get("media_url") + obj.pdc
+                  }));
+};
 
 const Lieu = mongoose.model("Lieu", lieuSchema);
 
