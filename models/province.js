b107541167c275fa16259aae7bdda86b8795687d
yuki-UwU <raoseta@gmail.com>
2023-08-01 17:17:31 +0300

    [DELETE] detailprovince

--- models/province.js
+++ models/province.js
@@ -8,12 +8,20 @@ const provinceSchema = new mongoose.Schema({
     minlength: 2,
     maxlength: 64
   },
+  description: {
+    type: String,
+    minlength: 2,
+    maxlength: 512
+  },
   pdp: {
     type: String
   },
   pdc: {
     type: String
-  }
+  },
+  liens: [String],
+  images: [String],
+  videos: [String]
 });
 
 provinceSchema.statics.get = async function (query) {
@@ -21,8 +29,19 @@ provinceSchema.statics.get = async function (query) {
   
   return lieus.map(obj => ({ ...obj, 
                     pdp: config.get("media_url") + obj.pdp,
-                    pdc: config.get("media_url") + obj.pdc
-                  }));
+                    pdc: config.get("media_url") + obj.pdc,
+                    videos: obj.videos.map(item => config.get("media_url") + item)
+  }));
+};
+
+provinceSchema.statics.getOne = async function (query) {
+  let province = await this.findOne(query).lean().exec();
+
+  return { ...province, 
+              pdp: config.get("media_url") + obj.pdp,
+              pdc: config.get("media_url") + obj.pdc,
+              videos:province.videos.map(item => config.get("media_url") + item) 
+  };
 };
 
 const Province = mongoose.model("Province", provinceSchema);
